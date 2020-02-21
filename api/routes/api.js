9ef6e0748947118f6327dca3440a0d9ecf31abc8
Garrett Parris <garrettp18@gmail.com>
2020-02-21 09:23:32 -0500

    seperate backend from frontend
    backend and fronted are now different repos

---
+++ api/routes/api.js
@@ -0,0 +1,38 @@
+var express = require('express');
+var router = express.Router();
+var mongoose = require('mongoose')
+mongoose.connect('mongodb://127.0.0.1:27017/inszndb', { useNewUrlParser: true })
+const db = mongoose.connection
+db.on('error', (error) => console.error(error))
+db.once('open', () => console.log('connected to database'))
+var Schema = mongoose.Schema;
+
+var schema = new Schema(
+    {
+        name: String,
+        months: [Number],
+    },
+    {
+        collection: 'fruit'
+    }
+)
+var Produce = mongoose.model('Produce', schema);
+
+
+router.get('/:id', async (req, res) =>
+{
+
+
+    try {
+        console.log(req.params.id)
+
+        const produce = await Produce.find({'months': req.params.id})
+        res.send(produce)
+        console.log(produce)
+      } catch (err) {
+        res.status(500).json({ message: err.message })
+    }
+    db.close()
+});
+
+module.exports = router;
---
+++ api/routes/index.js
@@ -0,0 +1,16 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+  router.get('/', async (req, res) => {
+    try {
+  
+    } catch (err) {
+  
+    }
+  })
+});
+
+module.exports = router;
