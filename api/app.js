db053a6eabf483f00dab6abfa2bf68c6a2c17c8d
Garrett Parris <garrettp18@gmail.com>
2020-03-15 14:19:33 -0400

    naming update

--- api/app.js
+++ api/app.js
@@ -10,7 +10,7 @@ var mongoose = require('mongoose')
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-var testAPIRouter = require("./routes/message");
+var messageRouter = require("./routes/message");
 var api = require("./routes/api");
 
 var app = express();
@@ -31,7 +31,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-app.use("/message", testAPIRouter);
+app.use("/message",cors(), messageRouter);
 app.use("/api", api);
 
 // catch 404 and forward to error handler
